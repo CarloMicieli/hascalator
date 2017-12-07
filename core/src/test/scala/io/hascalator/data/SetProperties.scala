b03afff2b8b5503fc2b6e89a66456c9d47651b0d
CarloMicieli <carlo.micieli@gmail.com>
2017-12-07 09:56:11 +0100

    Added union and intersection properties

--- core/src/test/scala/io/hascalator/data/SetProperties.scala
+++ core/src/test/scala/io/hascalator/data/SetProperties.scala
@@ -22,6 +22,8 @@ import org.scalacheck.Prop.forAll
 import tests.arbitrary.set._
 
 class SetProperties extends AbstractPropertySpec {
+  import setSymbols._
+
   property("size and isEmpty are consistent for sets") {
     check(forAll { (set: Set[Int]) =>
       {
@@ -38,4 +40,111 @@ class SetProperties extends AbstractPropertySpec {
       }
     })
   }
+
+  property("union: commutativity property") {
+    check(forAll { (A: Set[Int], B: Set[Int]) =>
+      {
+        (A ∪ B) === (B ∪ A)
+      }
+    })
+  }
+
+  property("union: associativity property") {
+    check(forAll { (A: Set[Int], B: Set[Int], C: Set[Int]) =>
+      {
+        ((A ∪ B) ∪ C) === (A ∪ (B ∪ C))
+      }
+    })
+  }
+
+  property("union: A ⊆ (A ∪ B)") {
+    check(forAll { (A: Set[Int], B: Set[Int]) =>
+      {
+        A ⊆ (A ∪ B)
+      }
+    })
+  }
+
+  property("union: A ∪ A = A") {
+    check(forAll { (A: Set[Int]) =>
+      {
+        (A ∪ A) === A
+      }
+    })
+  }
+
+  property("union: A ∪ ∅ = A") {
+    check(forAll { (A: Set[Int]) =>
+      {
+        (A ∪ ∅) === A
+      }
+    })
+  }
+
+  property("union: A ⊆ B if and only if A ∪ B = B.") {
+    check(forAll { (A: Set[Int], B: Set[Int]) =>
+      {
+        (A ⊆ B) === ((A ∪ B) == B)
+      }
+    })
+  }
+
+  property("intersection: A ∩ B = B ∩ A.") {
+    check(forAll { (A: Set[Int], B: Set[Int]) =>
+      {
+        (A ∩ B) === (B ∩ A)
+      }
+    })
+  }
+
+  property("intersection: A ∩ (B ∩ C) = (A ∩ B) ∩ C.") {
+    check(forAll { (A: Set[Int], B: Set[Int], C: Set[Int]) =>
+      {
+        (A ∩ (B ∩ C)) === ((A ∩ B) ∩ C)
+      }
+    })
+  }
+
+  property("intersection: A ∩ B ⊆ A.") {
+    check(forAll { (A: Set[Int], B: Set[Int]) =>
+      {
+        (A ∩ B) ⊆ A
+      }
+    })
+  }
+
+  property("intersection: A ∩ A = A.") {
+    check(forAll { (A: Set[Int]) =>
+      {
+        (A ∩ A) === A
+      }
+    })
+  }
+
+  property("intersection: A ∩ ∅ = ∅.") {
+    check(forAll { (A: Set[Int]) =>
+      {
+        (A ∩ ∅) === ∅
+      }
+    })
+  }
+
+  property("intersection: A ⊆ B if and only if A ∩ B = A.") {
+    check(forAll { (A: Set[Int], B: Set[Int]) =>
+      {
+        (A ⊆ B) === ((A ∩ B) == A)
+      }
+    })
+  }
+}
+
+object setSymbols {
+
+  implicit class SetExtensions[T](val A: Set[T]) extends AnyVal {
+    def ∪(B: Set[T])(implicit ord: Ord[T]): Set[T] = A union B
+    def ∩(B: Set[T])(implicit ord: Ord[T]): Set[T] = A intersection B
+    def ⊆(B: Set[T])(implicit ord: Ord[T]): Boolean = A isSubsetOf B
+  }
+
+  def ∅[A]: Set[A] = Set.empty
 }
